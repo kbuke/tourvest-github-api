f62a83d47c317b1ca764745f935cb71fe92d4887
Kaan Buke <kabuke13@gmail.com>
2025-11-10 10:25:23 +0200

    allows user to refresh the repo page and keep the data by using useParams

--- src/Pages/Repos/Repo.jsx
+++ src/Pages/Repos/Repo.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react"
+import { useOutletContext, useParams } from "react-router"
+import { useFetch } from "../../Hooks/useFetch"
+
 export function Repo(){
+    const [repos, setRepos] = useState([])
+    const [allRepos, setAllRepos] = useState([])
+    const [selectedRepo, setSelectedRepo] = useState(null)
+
+    const appData = useOutletContext()
+    const isLoading = appData?.isLoading
+    const setIsLoading = appData?.setIsLoading
+    const error = appData?.error
+    const setError = appData?.setError
+
+    const param = useParams()
+    const username = param?.username
+
+    useFetch(
+        username? `users/${username}/repos` : null, {
+            setData: (data) => {
+                setRepos(data)
+                setAllRepos(data)
+            },
+            setIsLoading,
+            setError,
+            onSuccess: null
+        }
+    )
+
+    const userRepo = repos[0]?.owner
+
     return(
-        <h1>
-            Haey you made it to the repo page
-        </h1>
+        <>
+        </>
     )
 }
