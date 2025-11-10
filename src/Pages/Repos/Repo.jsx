18a376b5da7e0e1e4d13d951d787e399b5cddf26
Kaan Buke <kabuke13@gmail.com>
2025-11-10 09:21:09 +0200

    add Repo page to the router

---
+++ src/Pages/Repos/Repo.jsx
@@ -0,0 +1,7 @@
+export function Repo(){
+    return(
+        <h1>
+            Haey you made it to the repo page
+        </h1>
+    )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 
 import App from "./App";
 import { Home } from "./Pages/Home/Home";
+import { Repo } from "./Pages/Repos/Repo";
 
 export const router = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
             {
                 path: "/",
                 element: <Home />
+            },
+            {
+                path: "/repos/:username",
+                element: <Repo />
             }
         ]
     }
