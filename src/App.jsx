543dbb4ce42849f7c7c783ddd0d74585b240a73e
Kaan Buke <kabuke13@gmail.com>
2025-11-10 08:24:53 +0200

    define states for props that will be passed down to children

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
 function App() {
   
+  const [repos, setRepos] = useState([])
+  const [isLoading, setIsLoading] = useState([])
+  const [error, setError] = useState([])
 
   return (
     <>
-      
+      <Outlet 
+        context = {
+          {
+            repos: repos, setRepos: setRepos,
+
+            isLoading: isLoading, setIsLoading: setIsLoading,
+            
+            error: error, setError: setError
+          }
+        }
+      />
     </>
   )
 }
